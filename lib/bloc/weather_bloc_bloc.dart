ae62ba78f2f7bf788ad834afd8c9a8991f4c7854
Gabriel <gabrielhenriquedps@gmail.com>
2023-10-18 11:27:55 -0300

    Adição de dependências, finalização da estrutura bloc

--- lib/bloc/weather_bloc_bloc.dart
+++ lib/bloc/weather_bloc_bloc.dart
@@ -1,14 +1,31 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:weather/weather.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'weather_bloc_event.dart';
 part 'weather_bloc_state.dart';
 
 class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
   WeatherBlocBloc() : super(WeatherBlocInitial()) {
-    on<WeatherBlocEvent>((event, emit) {
-      // TODO: implement event handler
+    on<FetchWeather>((event, emit) async {
+      emit(WeatherBlocLoading());
+      try {
+        await dotenv.load(fileName: ".env");
+        String apiKey = dotenv.env['API_KEY'] ?? 'Variavel não definida';
+
+        Position position = await Geolocator.getCurrentPosition();
+        WeatherFactory wf =
+            WeatherFactory(apiKey, language: Language.PORTUGUESE_BRAZIL);
+
+        Weather weather = await wf.currentWeatherByLocation(
+            position.latitude, position.longitude);
+        emit(WeatherBlocSuccess(weather));
+      } catch (e) {
+        print(e);
+        emit(WeatherBlocFailure());
+      }
     });
   }
 }
